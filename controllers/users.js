c1182aa4e6a525c2ac1eb0dfd773fedddcd71da6
PeterSonne <papa@deep2chill.de>
2020-02-27 13:17:37 +0800

    Unique Email Signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,25 @@
 // Require
-const Users = require('../models/users')
-const router = require('express').Router()
+const Users = require("../models/users");
+const router = require("express").Router();
 
 // Routes
-router.post('/signup', (req, res) => {})
-router.post('/login', (req, res) => {})
+router.post("/signup", (req, res) => {
+  // check if mail exists already
+  console.log(req.body);
+  Users.findOne({ email: req.body.email })
+    .then(usr => {
+      if (usr !== null) {
+        res.send("E-Mail already exists!");
+      } else {
+        // create user
+        Users.create(req.body)
+          .then(usr => res.send(usr))
+          .catch(err => res.send(err));
+      }
+    })
+    .catch(err => console.log(err));
+});
+router.post("/login", (req, res) => {});
 
 // Export
-module.exports = router
+module.exports = router;
